52c2455f890a299eae5748cd11dabb8ce560fa67
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2022-10-22 10:49:17 -0500

    Format three main tabs

--- ionic/max-cash/src/pages/Tab2.tsx
+++ ionic/max-cash/src/pages/Tab2.tsx
@@ -7,16 +7,16 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>MaxCash Title</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">MaxCash Toolbar</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="MaxCash page" />
       </IonContent>
     </IonPage>
   );
