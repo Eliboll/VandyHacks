7160339302619668f2e608881738b3dbb4a771a4
Eliboll <eetes77@gmail.com>
2022-10-22 10:53:54 -0500

    added sql schematic

---
+++ server/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cards;
+
+CREATE TABLE users (
+    userid INTEGER PRIMARY KEY AUTOINCREMENT,
+    ccid TEXT NOT NULL
+);
+
+CREATE TABLE cards (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    specials TEXT NOT NULL,
+    categories TEXT NOT NULL,
+    base INTEGER NOT NULL,
+    name TEXT not NULL,
+    company TEXT not NULL
+);
